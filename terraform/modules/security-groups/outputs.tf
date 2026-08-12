93fc931921bc4ac22a7c6d4379b0c055197445c2
satish <pathadesatish0@gmail.com>
2026-08-12 20:12:00 +0530

    all security groups outputs

---
+++ terraform/modules/security-groups/outputs.tf
@@ -0,0 +1,27 @@
+output "public_alb_sg_id" {
+  value = aws_security_group.public_alb.id
+}
+
+output "web_sg_id" {
+  value = aws_security_group.web.id
+}
+
+output "internal_alb_sg_id" {
+  value = aws_security_group.internal_alb.id
+}
+
+output "app_sg_id" {
+  value = aws_security_group.app.id
+}
+
+output "rds_sg_id" {
+  value = aws_security_group.rds.id
+}
+
+output "cicd_sg_id" {
+  value = aws_security_group.cicd.id
+}
+
+output "k8s_common_sg_id" {
+  value = aws_security_group.k8s_common.id
+}
