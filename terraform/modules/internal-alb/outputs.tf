900df0c8c8c160a49cf70a29b68da350c5af67a5
satish <pathadesatish0@gmail.com>
2026-08-16 23:23:59 +0530

    internal alb outputs

---
+++ terraform/modules/internal-alb/outputs.tf
@@ -0,0 +1,15 @@
+output "alb_dns_name" {
+  value = aws_lb.this.dns_name
+}
+
+output "target_group_arn" {
+  value = aws_lb_target_group.app_tg.arn
+}
+
+output "alb_arn_suffix" {
+  value = aws_lb.this.arn_suffix
+}
+
+output "target_group_arn_suffix" {
+  value = aws_lb_target_group.app_tg.arn_suffix
+}
